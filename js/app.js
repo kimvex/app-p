b9ff395bb25b68f13f30ea93ce967b1790a58682
Benjamin <chomi.numb.94@gmail.com>
2015-12-17 05:27:20 -0600

    Agregamos el proyecto

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+	function conectar(e){
+		document.getElementById('action').innerHTML = '<object class="servervid" data="http://192.168.0.101:5000" type="text/html"></object>';
+	}
+	conectar();
+});
