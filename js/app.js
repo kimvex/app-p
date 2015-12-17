8952e8be471cafe8e61e8faef417c837ad5ebf6f
Benjamin <chomi.numb.94@gmail.com>
2015-12-17 06:44:14 -0600

    Agregamos nuevas funciones

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	function conectar(e){
-		document.getElementById('action').innerHTML = '<object class="servervid" data="http://192.168.0.101:5000" type="text/html"></object>';
+    var ip = '192.168.0.101:5000';
+		document.getElementById('action').innerHTML = '<object class="servervid" data="http://'+ ip +'" type="text/html"></object>';
 	}
 	conectar();
 });
